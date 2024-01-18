5e3cdcc83b283db3306d6a6c7eee2021b28415fc
Selam Ararsa <selamkedir@icloud.com>
2024-01-18 17:32:19 +0000

    deployment

--- attract/pages/_document.js
+++ attract/pages/_document.js
@@ -1,15 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <link rel="shortcut icon" href="/static/favicon.ico" />
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
